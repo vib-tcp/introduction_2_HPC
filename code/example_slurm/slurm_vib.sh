a5925b9eb804037d9fef6dd724d3da1e1f0dc601
Boris Depoortere <118811684+B0r1sD@users.noreply.github.com>
2025-10-16 17:37:33 +0200

    Modify job time and enhance comments in slurm_vib.sh
    Updated job time limit and added comments for clarity.

--- code/example_slurm/slurm_vib.sh
+++ code/example_slurm/slurm_vib.sh
@@ -2,15 +2,15 @@
 #SBATCH --job-name=test_job_submission
 #SBATCH --partition=debug_28C_56T_750GB
 #SBATCH --mem=8G
-#SBATCH --time=00:30:00
+#SBATCH --time=00:05:00
 
 # Define variables
 DOCKER_IMG="docker://hello-world"
 
-# Practice best practice by purging all loaded modules first
+# Best practice to purge all loaded modules first
 module purge
 
-# Load necessary modules (none needed for this example)
+# Load necessary modules (none needed for this example), example line below:
 # module load Python/3.13.1-GCCcore-14.2.0
 
 # Pull minimal Docker image using Singularity
@@ -19,3 +19,9 @@ singularity pull $DOCKER_IMG
 # Sleep for 20 seconds so there is time to see the job via `squeue`
 echo 'Going to sleep for 20 seconds... ZZZzzzzz....'
 sleep 20
+
+# Execute the hello world image. The Docker deamon will create a new container wich has an executable inside that will display text in your terminal:
+singularity run hello-world_latest.sif
+
+# Submit this Slurm launch script using the following command (from the same directory this file is in):
+# sbatch slurm_vib.sh
